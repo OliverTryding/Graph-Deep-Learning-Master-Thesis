d99ce78051c27da5f618cf7fdf9d38a95577b8ea
OliverTryding <oliver.tryding@gmail.com>
2024-04-23 23:11:05 +0200

    Updated all networks to use HGNNConv instead of simple message passing

--- EnvironmentNetwork.py
+++ EnvironmentNetwork.py
@@ -4,6 +4,33 @@ import torch.nn.functional as F
 
 import dhg
 from dhg import Hypergraph
+from dhg.nn import HGNNConv
+    
+# class environment_network(nn.Module):
+#     def __init__(self, num_features: int, G: Hypergraph, aggregation: str = "mean"):
+#         super().__init__()
+#         self.G = G
+#         self.lin_message = nn.Linear(num_features, num_features, bias=False)
+#         self.lin_update = nn.Linear(num_features, num_features, bias=True)
+#         self.aggregation = aggregation
+
+#     def forward(self, x, action, edge_weight=(None,None)):
+#         # Unless the action is S or B, the sent message is zero
+#         send = action[:, 0] + action[:, 2]
+#         # Unless the action is S or L, the received message is zero
+#         receive = action[:, 0] + action[:, 1]
+        
+#         # Compute the messages
+#         m_ji = self.lin_message(x)
+#         m_ji = torch.nn.GELU()(m_ji)
+#         m_ji = m_ji * send.view(-1, 1) # mask the messages
+
+#         # Aggregate the messages
+#         m_i = self.G.v2v(m_ji, self.aggregation, v2e_weight=edge_weight[0], e2v_weight=edge_weight[1])
+#         m_i = m_i * receive.view(-1, 1) # mask the messages
+
+#         h_i = torch.nn.GELU()(self.lin_update(x) + m_i)
+#         return h_i
     
 class environment_network(nn.Module):
     def __init__(self, num_features: int, G: Hypergraph, aggregation: str = "mean"):
@@ -12,19 +39,19 @@ class environment_network(nn.Module):
         self.lin_message = nn.Linear(num_features, num_features, bias=False)
         self.lin_update = nn.Linear(num_features, num_features, bias=True)
         self.aggregation = aggregation
+        self.conv = HGNNConv(num_features, num_features, bias=False)
 
     def forward(self, x, action, edge_weight=(None,None)):
         # Unless the action is S or B, the sent message is zero
         send = action[:, 0] + action[:, 2]
         # Unless the action is S or L, the received message is zero
         receive = action[:, 0] + action[:, 1]
-        
-        # Compute the messages
-        m_ji = self.lin_message(x)
-        m_ji = m_ji * send.view(-1, 1) # mask the messages
+
+        # Zero out features where send is zero
+        x_masked = x * send.view(-1, 1)
 
         # Aggregate the messages
-        m_i = self.G.v2v(m_ji, self.aggregation, v2e_weight=edge_weight[0], e2v_weight=edge_weight[1])
+        m_i = self.conv(x_masked, self.G)
         m_i = m_i * receive.view(-1, 1) # mask the messages
 
         h_i = torch.nn.GELU()(self.lin_update(x) + m_i)
--- HCoGNN.py
+++ HCoGNN.py
@@ -10,26 +10,30 @@ from ActionNetwork import action_network
 from EnvironmentNetwork import environment_network
 
 class HCoGNN_node_classifier(nn.Module):
-    def __init__(self, num_features: int, num_classes: int, G: Graph, num_iterations: int):
+    def __init__(self, num_features: int, num_classes: int, G: Graph, action_aggr: str = 'mean', environment_aggr: str = 'mean', num_iterations: int = 1):
         super().__init__()
         self.classifier = nn.Linear(num_features, num_classes)
         self.G = G
         self.num_iterations = num_iterations
-        self.action_net = action_network(num_features, G)
-        self.environment_net = environment_network(num_features, G)
+        self.action_net = action_network(num_features, G, action_aggr)
+        self.environment_net = environment_network(num_features, G, environment_aggr)
         self.action_history = []
 
-    def forward(self, x, edge_weight=(None,None)):          
+    def forward(self, x, edge_weight=(None,None)):
+        action = torch.zeros(x.shape[0], 4).to(x.device)
+        action[:, 0] = 1
+        if self.eval():
+            self.action_history.append(torch.argmax(action, dim=1))
         # Pass the input through the MPNN for a number of iterations
         for i in range(self.num_iterations):
+            # Update the node features
+            x = self.environment_net(x, action, edge_weight)
             # Determine the actions
             p_i = self.action_net(x, edge_weight) # probabilities for action selection {S, L, B, I}
             # Sample an action using the straight-through Gumbel-softmax estimator
             action = F.gumbel_softmax(p_i, hard=True)
             if self.eval():
                 self.action_history.append(torch.argmax(action, dim=1))
-            # Update the node features
-            x = self.environment_net(x, action, edge_weight)
         # Pass the output through the classifier
         out = self.classifier(x)
         # Apply softmax to get probabilities

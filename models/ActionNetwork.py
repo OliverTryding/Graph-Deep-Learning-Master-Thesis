6db756e7e03a076bcfad30ad012e5f20e422a6d2
OliverTryding <oliver.tryding@gmail.com>
2024-05-25 21:52:13 +0200

    Remove comments

--- models/ActionNetwork.py
+++ models/ActionNetwork.py
@@ -27,20 +27,3 @@ class action_network(torch.nn.Module):
         h_i = torch.nn.GELU()(self.lin_update(x) + m_i)
         h_i = F.log_softmax(h_i, dim=1)
         return h_i
-    
-# class action_network(torch.nn.Module):
-#     """Classify a node feature vector into {S,L,B,I}."""
-#     def __init__(self, num_features: int, G: Hypergraph, aggregation: str = "mean", dropout: float = 0.3):
-#         super().__init__()
-#         self.lin_update = nn.Linear(num_features, 4, bias=True)
-#         self.G = G
-#         self.aggregation = aggregation
-#         self.conv = HGNNConv(num_features, 4, bias=False, drop_rate=dropout)
-
-#     def forward(self, x, edge_weight=(None,None)):
-#         # Aggregate the messages
-#         m_i = self.conv(x, self.G)
-
-#         h_i = torch.nn.GELU()(self.lin_update(x) + m_i)
-#         h_i = F.log_softmax(h_i, dim=1)
-#         return h_i
--- models/EnvironmentNetwork.py
+++ models/EnvironmentNetwork.py
@@ -33,27 +33,3 @@ class environment_network(nn.Module):
 
         h_i = torch.nn.GELU()(self.lin_update(x) + m_i)
         return h_i
-    
-# class environment_network(nn.Module):
-#     def __init__(self, num_features: int, G: Hypergraph, aggregation: str = "mean", dropout: float = 0.3):
-#         super().__init__()
-#         self.G = G
-#         self.lin_update = nn.Linear(num_features, num_features, bias=True)
-#         self.aggregation = aggregation
-#         self.conv = HGNNConv(num_features, num_features, bias=False, drop_rate=dropout)
-
-#     def forward(self, x, action, edge_weight=(None,None)):
-#         # Unless the action is S or B, the sent message is zero
-#         send = action[:, 0] + action[:, 2]
-#         # Unless the action is S or L, the received message is zero
-#         receive = action[:, 0] + action[:, 1]
-
-#         # Zero out features where send is zero
-#         x_masked = x * send.view(-1, 1)
-
-#         # Aggregate the messages
-#         m_i = self.conv(x_masked, self.G)
-#         m_i = m_i * receive.view(-1, 1) # mask the messages
-
-#         h_i = torch.nn.GELU()(self.lin_update(x) + m_i)
-#         return h_i

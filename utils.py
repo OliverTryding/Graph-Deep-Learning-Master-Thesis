8a1e0d069938241d8fcf58181e53372fdf3029d1
OliverTryding <oliver.tryding@gmail.com>
2024-06-24 12:12:00 +0200

    Restructured code

--- utils.py
+++ utils.py
@@ -1,4 +1,118 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn.functional as F
+from sklearn.metrics import confusion_matrix
+
+import dhg
+from dhg import Graph
+from dhg import Hypergraph
+
+def random_walk_matrix(G):
+    deg = G.deg_v
+    v2e_weight = torch.ones_like(G.v2e_weight, dtype=torch.float32)
+    e2v_weight = torch.ones_like(G.e2v_weight, dtype=torch.float32)
+    for i, v in enumerate(G.v2e_src):
+        v2e_weight[i] = 1.0 / deg[v]
+    return v2e_weight, e2v_weight
+
+bfgs_loss = torch.inf
+
+# BFGS Closure
+def closure(model, optimizer, X, G, labels, train_mask):
+    optimizer.zero_grad()
+    out = model(X, G)
+    loss = F.cross_entropy(out[train_mask], labels[train_mask])
+    loss.backward()
+    global bfgs_loss 
+    bfgs_loss = loss.item()
+    return loss
+
+# Training loop
+def train(model, optimizer, X, G, labels, train_mask):
+    model.train()
+    if optimizer.__class__.__name__ == 'LBFGS':
+        optimizer.step(closure=lambda: closure(model, optimizer, X, G, labels, train_mask))
+        return bfgs_loss
+    else:
+        optimizer.zero_grad()
+        out = model(X, G)
+        #loss = F.nll_loss(out[train_mask], labels[train_mask])
+        loss = F.cross_entropy(out[train_mask], labels[train_mask])
+        loss.backward()
+        optimizer.step()
+        return loss.item()
+
+def validate(model, X, G, labels, train_mask, val_mask):
+    model.eval()
+    with torch.no_grad():
+        logits = model(X, G) # Log probabilities
+
+        # Train accuracy
+        train_logits = logits[train_mask] # Log probabilities of train nodes
+        train_labels = labels[train_mask] # True labels of train nodes
+        train_pred = train_logits.max(1)[1] # Predicted labels
+        train_correct = train_pred.eq(train_labels).sum().item() # Number of correctly classified nodes
+        train_accuracy = train_correct / train_mask.sum().item() # Accuracy
+
+        # Validation accuracy
+        val_logits = logits[val_mask] # Log probabilities of validation nodes
+        val_labels = labels[val_mask] # True labels of validation nodes
+        val_pred = val_logits.max(1)[1] # Predicted labels
+        val_correct = val_pred.eq(val_labels).sum().item() # Number of correctly classified nodes
+        val_accuracy = val_correct / val_mask.sum().item() # Accuracy
+            
+    return train_accuracy, val_accuracy
+
+# Testing function
+def test(model, X, G, labels, test_mask):
+    model.eval()
+    with torch.no_grad():
+        logits = model(X, G)
+        test_logits = logits[test_mask] # Log probabilities of test nodes
+        test_labels = labels[test_mask] # True labels of test nodes
+        pred = test_logits.max(1)[1] # Predicted labels
+        correct = pred.eq(test_labels).sum().item() # Number of correctly classified nodes
+        accuracy = correct / test_mask.sum().item() # Accuracy
+
+        if hasattr(model, 'action_history'):
+            for layer in range(model.num_iterations):
+                actions = model.action_history[layer].cpu()
+
+                # Convert list to numpy array
+                actions_array = np.array(actions)
+
+                # Compute the ratio
+                ratio = np.bincount(actions_array, minlength=4) / len(actions_array)
+                ratio = np.round(ratio, 4)
+
+                print(f"Ratio of actions in layer {layer}: {ratio} (S, L, B, I)")
+                        
+    return accuracy, pred
+
+def visualize_results(model, X, G, labels, test_mask):
+    model.eval()
+    with torch.no_grad():
+        logits = model(X, G)
+        test_logits = logits[test_mask] # Log probabilities of test nodes
+        test_labels = labels[test_mask] # True labels of test nodes
+        pred = test_logits.max(1)[1] # Predicted labels
+
+        cm = confusion_matrix(test_labels.cpu(), pred.cpu())
+
+        # Normalize confusion matrix
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+        fig, ax = plt.subplots(1, 2, figsize=(20,10))
+
+        # Plot confusion matrix
+        im = ax[0].matshow(cm, cmap=plt.cm.Blues)
+        plt.colorbar(im, ax=ax[0])
+        ax[0].set_title('Confusion matrix')
+        ax[0].set_ylabel('True label')
+        ax[0].set_xlabel('Predicted label')
+
+        plt.show()
 
 class EarlyStopping:
     def __init__(self, patience=5, mode='min', delta=0.0):
@@ -8,22 +122,27 @@ class EarlyStopping:
         self.counter = 0
         self.best_score = None
         self.stop = False
+        self.best_model = None
 
         if self.mode == 'min':
             self.best_score = np.Inf
         else:
             self.best_score = -np.Inf
 
-    def __call__(self, score):
+    def __call__(self, model, score):
         if self.mode == 'min':
-            if score < self.best_score + self.delta:
+            if score < self.best_score:
                 self.best_score = score
+                self.best_model = model.state_dict()
+            if score < self.best_score + self.delta:
                 self.counter = 0
             else:
                 self.counter += 1
         else:
-            if score > self.best_score + self.delta:
+            if score > self.best_score:
                 self.best_score = score
+                self.best_model = model.state_dict()
+            if score > self.best_score + self.delta:
                 self.counter = 0
             else:
                 self.counter += 1

214912f09220d64f9129798e47a2a6d7348d2a98
OliverTryding <oliver.tryding@gmail.com>
2024-06-24 12:12:51 +0200

    Standardized input format for forward pass in models.

--- models/ActionNetwork.py
+++ models/ActionNetwork.py
@@ -8,9 +8,10 @@ from dhg.nn import HGNNConv
     
 class action_network(torch.nn.Module):
     """Classify a node feature vector into {S,L,B,I}."""
-    def __init__(self, num_features: int, G: Hypergraph, aggregation: str = "mean", activation = nn.ReLU(), layer_dims: list = [], dropout: float = 0.3):
+    def __init__(self, num_features: int, aggregation: str = "mean", activation = nn.ReLU(), layer_dims: list = [], depth: int = 1, dropout: float = 0.3):
         super().__init__()
 
+        self.depth = depth
         self.layers = nn.ModuleList()
         self.activation = activation
         layer_dims = [num_features] + layer_dims + [num_features]
@@ -18,16 +19,25 @@ class action_network(torch.nn.Module):
             self.layers.append(nn.Linear(layer_dims[i], layer_dims[i+1]))
             self.layers.append(activation)
         self.lin_update = nn.Linear(num_features, num_features, bias=True)
+        self.action_layer = nn.Linear(num_features, 4)
         self.dropout = nn.Dropout(dropout)
-        self.G = G
         self.aggregation = aggregation
 
-    def forward(self, x, edge_weight=(None,None)):
-        x = self.update(x, edge_weight)
-        h_i = F.log_softmax(x, dim=1)
-        return h_i
+    def forward(self, x, G: Hypergraph):
+        if self.depth == 0:
+            action = torch.zeros(x.shape[0], 4).to(x.device)
+            action[:, 0] = 1
+            return action
+        for _ in range(self.depth):
+            x = self.update(x, G)
+        a_i = self.action_layer(x)
+
+        # Compute the action probabilities
+        p_i = F.log_softmax(a_i, dim=1) # probabilities for action selection {S, L, B, I}
+
+        return p_i
     
-    def update(self, x, edge_weight=(None,None)):
+    def update(self, x, G: Hypergraph):
         # Compute the messages
         m_ji = x
         for layer in self.layers:
@@ -35,7 +45,7 @@ class action_network(torch.nn.Module):
         m_ji = self.dropout(m_ji)
 
         # Aggregate the messages
-        m_i = self.G.v2v(m_ji, self.aggregation, v2e_weight=edge_weight[0], e2v_weight=edge_weight[1])
+        m_i = G.v2v(m_ji, self.aggregation)
         h_i = self.lin_update(x) + m_i
         h_i = self.activation(h_i)
 
--- models/EnvironmentNetwork.py
+++ models/EnvironmentNetwork.py
@@ -7,9 +7,10 @@ from dhg import Hypergraph
 from dhg.nn import HGNNConv
     
 class environment_network(nn.Module):
-    def __init__(self, num_features: int, G: Hypergraph, aggregation: str = "mean", activation = nn.ReLU(), layer_dims: list = [], dropout: float = 0.3):
+    def __init__(self, num_features: int, aggregation: str = "mean", activation = nn.ReLU(), layer_dims: list = [], depth: int = 1, dropout: float = 0.3):
         super().__init__()
 
+        self.depth = depth
         self.layers = nn.ModuleList()
         self.activation = activation
         layer_dims = [num_features] + layer_dims + [num_features]
@@ -18,20 +19,20 @@ class environment_network(nn.Module):
             self.layers.append(activation)
         self.lin_update = nn.Linear(num_features, num_features, bias=True)
         self.dropout = nn.Dropout(dropout)
-        self.G = G
         self.aggregation = aggregation
 
-    def forward(self, x, action, edge_weight=(None,None)):
+    def forward(self, x, action, G: Hypergraph):
         # Unless the action is S or B, the sent message is zero
         send = action[:, 0] + action[:, 2]
         # Unless the action is S or L, the received message is zero
         receive = action[:, 0] + action[:, 1]
 
-        x = self.update(x, edge_weight, send, receive)
+        for _ in range(self.depth):
+            x = self.update(x, G, send, receive)
 
         return x
     
-    def update(self, x, edge_weight=(None,None), send=None, receive=None):
+    def update(self, x, G: Hypergraph, send=None, receive=None):
         # Compute the messages
         m_ji = x
         m_ji = m_ji * send.view(-1, 1) # mask the messages
@@ -39,7 +40,7 @@ class environment_network(nn.Module):
             m_ji = layer(m_ji)
         m_ji = self.dropout(m_ji)
         # Aggregate the messages
-        m_i = self.G.v2v(m_ji, self.aggregation, v2e_weight=edge_weight[0], e2v_weight=edge_weight[1])
+        m_i = G.v2v(m_ji, self.aggregation)
         m_i = m_i * receive.view(-1, 1) # mask the messages
         h_i = self.lin_update(x) + m_i
         h_i = self.activation(h_i)
